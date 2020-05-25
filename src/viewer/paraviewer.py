8f5e0633b8a287c9f5d1d339c9de6f30999fd6fc
Ignacio Bejarano <ignbejarano@gmail.com>
2020-05-25 14:24:29 -0300

    add new xml generator class impl

--- src/viewer/paraviewer.py
+++ src/viewer/paraviewer.py
@@ -14,3 +14,440 @@ class Paraviewer:
         # self.fullCoordVec.view()
         ViewHDF5.view(obj=coords)  # Put PETSc object into viewer
         ViewHDF5.destroy()
+
+    def saveVec(self, vec, timeStep=None):
+        """Save the vector."""
+        self.logger.debug("saveVec %s" % name)
+        ViewHDF5 = PETSc.ViewerHDF5()     # Init. Viewer
+
+        name = vec.getName()
+        if timeStep is None:
+            ViewHDF5.create(name + '.h5', mode=PETSc.Viewer.Mode.WRITE,
+                            comm=self.comm)
+        else:
+            ViewHDF5.create(name + '-%04d.h5' % timeStep,
+                            mode=PETSc.Viewer.Mode.WRITE, comm=self.comm)
+        ViewHDF5.pushGroup('/fields')
+        ViewHDF5.view(obj=vec)   # Put PETSc object into the viewer
+        ViewHDF5.destroy()            # Destroy Viewer
+
+    def saveXmf(self, name, numItems=None, meshName='mesh'):
+        """Save the XMF related to the total number of nodes."""
+        self.logger.debug("saveXmf")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts).4E" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XY">
+        <DataItem Dimensions="%(nodesX2)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+
+    <Attribute Name="%(name)s" AttributeType="Scalar" Center="Node">
+        <DataItem NumberType="Float" Format="HDF" Dimensions="%(nodes)d">
+            %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+        </DataItem>
+    </Attribute>
+</Grid>
+
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+    def saveXmfMult(self, name, numItems=None, meshName='mesh', variables=None):
+        """Save the XMF related to the total number of nodes."""
+        self.logger.debug("saveXmf")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts).4E" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XY">
+        <DataItem Dimensions="%(nodesX2)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+    """
+        attributes = """
+    <Attribute Name="%(name)s" AttributeType="Scalar" Center="Node">
+        <DataItem NumberType="Float" Format="HDF" Dimensions="%(nodes)d">
+            %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+        </DataItem>
+    </Attribute>
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+        if numItems is None:
+            numItems = self.nnodetot
+
+        with open(name+'.xmf', 'w') as fout:
+            fout.write(header)
+            if not self._ts:
+                self._ts = [0]
+            for i, ts in enumerate(self._ts):
+                fout.write(grid % { 'ts': ts,
+                                    'nodes': numItems,
+                                    'nodesX2': numItems * 2,
+                                    'meshName': meshName
+                                    })
+                for var in variables:
+                    fout.write(attributes % {
+                        'nodes': numItems,
+                        'stepnum': i,
+                        'name': var,
+                        })
+                fout.write("""</Grid>
+""")
+            fout.write(footer)
+
+    def saveXmf2(self, name, numItems=None, meshName='mesh'):
+        """Save the XMF related to the total number of nodes."""
+        self.logger.debug("saveXmf2")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts)s" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XY">
+        <DataItem Dimensions="%(nodesX2)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+
+    <Attribute Name="%(name)s" AttributeType="Vector" Center="Node">
+        <DataItem ItemType="Function" Dimensions="%(nodes)d 3"
+                Function="JOIN($0, $1, 0*$1)">
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sX">
+                <DataItem Dimensions="3 1" Format="XML">
+                    0
+                    2
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX2)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sY">
+                <DataItem Dimensions="3 1" Format="XML">
+                    1
+                    2
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX2)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+        </DataItem>
+    </Attribute>
+</Grid>
+
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+        if numItems is None:
+            numItems = self.nnodetot
+
+        with open(name+'.xmf', 'w') as fout:
+            fout.write(header)
+            if((not hasattr(self, '_ts')) or (not self._ts)):
+                self._ts = [0]
+            for i, ts in enumerate(self._ts):
+                fout.write(grid % {'ts': ts, 'nodes': numItems,
+                                   'nodesX2': numItems * 2, 'stepnum': i,
+                                   'name': name, 'meshName': meshName})
+            fout.write(footer)
+
+    def saveXmf2Mult(self, name, numItems=None, meshName='mesh', variables=None):
+        """Save the XMF related to the total number of nodes."""
+        self.logger.debug("saveXmf2")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts)s" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XY">
+        <DataItem Dimensions="%(nodesX2)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+    """
+        attributes = """
+    <Attribute Name="%(name)s" AttributeType="Vector" Center="Node">
+        <DataItem ItemType="Function" Dimensions="%(nodes)d 3"
+                Function="JOIN($0, $1, 0*$1)">
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sX">
+                <DataItem Dimensions="3 1" Format="XML">
+                    0
+                    2
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX2)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sY">
+                <DataItem Dimensions="3 1" Format="XML">
+                    1
+                    2
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX2)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+        </DataItem>
+    </Attribute>
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+        if numItems is None:
+            numItems = self.nnodetot
+
+        with open(name+'.xmf', 'w') as fout:
+            fout.write(header)
+            if not self._ts:
+                self._ts = [0]
+            for i, ts in enumerate(self._ts):
+                fout.write(grid % { 'ts': ts,
+                                    'nodes': numItems,
+                                    'nodesX2': numItems * 2,
+                                    'meshName': meshName
+                                    })
+                for var in variables:
+                    fout.write(attributes % {
+                        'nodes': numItems,
+                        'nodesX2': numItems * 2,
+                        'stepnum': i,
+                        'name': var,
+                        })
+                fout.write("""</Grid>
+""")
+            fout.write(footer)
+
+    def saveXmf3(self, name, numItems=None, meshName='mesh'):
+        """Save the XMF file for attributes with 3 components."""
+        self.logger.debug("saveXmf3")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts)s" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XYZ">
+        <DataItem Dimensions="%(nodesX3)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+
+    <Attribute Name="%(name)s" AttributeType="Vector" Center="Node">
+        <DataItem ItemType="Function" Dimensions="%(nodes)d 3"
+                Function="JOIN($0, $1, $2)">
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sX">
+                <DataItem Dimensions="3 1" Format="XML">
+                    0
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sY">
+                <DataItem Dimensions="3 1" Format="XML">
+                    1
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sZ">
+                <DataItem Dimensions="3 1" Format="XML">
+                    2
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+        </DataItem>
+    </Attribute>
+</Grid>
+
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+        if numItems is None:
+            numItems = self.nnodetot
+
+        with open(name+'.xmf', 'w') as fout:
+            fout.write(header)
+            if not self._ts:
+                self._ts = [0]
+            for i, ts in enumerate(self._ts):
+                fout.write(grid % {'ts': ts, 'nodes': numItems,
+                                   'nodesX2': numItems * 2,
+                                   'nodesX3': numItems * 3, 'stepnum': i,
+                                   'name': name, 'meshName': meshName})
+            fout.write(footer)
+
+
+    def saveXmf3Mult(self, name, numItems=None, meshName='mesh', variables=None):
+        """Save the XMF file for attributes with 3 components."""
+        self.logger.debug("saveXmf3")
+        header = """<?xml version="1.0" ?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0">
+<Domain>
+    <Grid Name="TimeSeries" GridType="Collection" CollectionType="Temporal">
+
+"""
+        grid = """        <Grid Name="mesh1" GridType="Uniform">
+    <Time Value="%(ts)s" />
+    <Topology TopologyType="Polyvertex" Dimensions="%(nodes)d"/>
+    <Geometry GeometryType="XYZ">
+        <DataItem Dimensions="%(nodesX3)d" NumberType="Float" Format="HDF">
+            %(meshName)s.h5:/fields/%(meshName)s
+        </DataItem>
+    </Geometry>
+"""
+        attributes ="""     <Attribute Name="%(name)s" AttributeType="Vector" Center="Node">
+        <DataItem ItemType="Function" Dimensions="%(nodes)d 3"
+                Function="JOIN($0, $1, $2)">
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sX">
+                <DataItem Dimensions="3 1" Format="XML">
+                    0
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sY">
+                <DataItem Dimensions="3 1" Format="XML">
+                    1
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+            <DataItem ItemType="HyperSlab" Dimensions="%(nodes)d"
+                    Name="%(name)sZ">
+                <DataItem Dimensions="3 1" Format="XML">
+                    2
+                    3
+                    %(nodes)d
+                </DataItem>
+                <DataItem Dimensions="%(nodesX3)d" NumberType="Float"
+                        Format="HDF">
+                    %(name)s-%(stepnum)04d.h5:/fields/%(name)s
+                </DataItem>
+            </DataItem>
+
+        </DataItem>
+    </Attribute>
+"""
+        footer = """    </Grid>
+</Domain>
+</Xdmf>
+"""
+
+        if numItems is None:
+            numItems = self.nnodetot
+
+        with open(name+'.xmf', 'w') as fout:
+            fout.write(header)
+            if not self._ts:
+                self._ts = [0]
+            for i, ts in enumerate(self._ts):
+                fout.write(grid % { 'ts': ts,
+                                    'nodes': numItems,
+                                    'nodesX3': numItems * 3,
+                                    'meshName': meshName
+                                    })
+                for var in variables:
+                    fout.write(attributes % {
+                        'nodes': numItems,
+                        'nodesX3': numItems * 3,
+                        'stepnum': i,
+                        'name': var,
+                        })
+                fout.write("""</Grid>
+""")
+            fout.write(footer)
---
+++ src/viewer/xml_generator.py
@@ -0,0 +1,37 @@
+from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+from xml.dom import minidom
+
+class XmlGenerator(object):
+    def __init__(self):
+        pass
+
+    def getXMLTemplate(self):
+        root = Element('Xdmf')
+        root.set('Version', '2.0')
+        # comment = Comment('Generated for PyMOTW')
+        # top.append(comment)
+        gridTime = Element('Grid')
+        gridTime.set('Name', 'mesh1')
+        gridTime.set('GridType', "uniform")
+        domain = SubElement(root, 'Domain')
+        grid = SubElement(domain, 'Grid')
+        grid.set('Name',"TimeSeries")
+        grid.set('GridType',"Collection")
+        grid.set('CollectionType',"Temporal")
+        
+        return root
+
+    def printMesh(self,):
+
+
+    def prettify(self, elem):
+        """Return a pretty-printed XML string for the Element.
+        """
+        rough_string = tostring(elem, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="  ")
+
+a = XmlGenerator()
+root = a.getXMLTemplate()
+print(b)
+
